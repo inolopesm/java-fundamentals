112c7c48f2f74028add4a8d5700b82abe933acda
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-10 20:32:17 -0300

    person class

--- src/main/java/com/fundamentals/Main.java
+++ src/main/java/com/fundamentals/Main.java
@@ -1,12 +1,14 @@
 package com.fundamentals;
 import java.util.Scanner;
+import com.fundamentals.entity.Person;
 
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please insert your name:");
     String name = scanner.nextLine();
-    System.out.println("Hello, " + name + "!");
+    Person person = new Person(name);
+    System.out.println("Hello, " + person.getName() + "!");
     scanner.close();
   }
 }
