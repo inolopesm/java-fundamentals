c35ff288734160d4ed60c6b7563c2d2a31fe1d9d
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-10 20:40:20 -0300

    try catch

--- src/main/java/com/fundamentals/Main.java
+++ src/main/java/com/fundamentals/Main.java
@@ -5,12 +5,18 @@ import com.fundamentals.entity.Person;
 public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Please insert your name:");
-    String name = scanner.nextLine();
-    System.out.println("Please insert your age:");
-    int age = Integer.parseInt(scanner.nextLine());
-    Person person = new Person(name, age);
-    System.out.println("Hello, " + person.getName() + " with age " + person.age + "!");
-    scanner.close();
+
+    try {
+      System.out.println("Please insert your name:");
+      String name = scanner.nextLine();
+      System.out.println("Please insert your age:");
+      int age = Integer.parseInt(scanner.nextLine());
+      Person person = new Person(name, age);
+      System.out.println("Hello, " + person.getName() + " with age " + person.age + "!");
+    } catch (NumberFormatException e) {
+      System.out.println("Ouch! Invalid number");
+    } finally {
+      scanner.close();
+    }
   }
 }
