7504f6a15ca6460f0da3585b0e7163b1555869a1
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-10 20:25:48 -0300

    name input

--- src/main/java/com/fundamentals/Main.java
+++ src/main/java/com/fundamentals/Main.java
@@ -1,7 +1,11 @@
 package com.fundamentals;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please insert your name:");
+    String name = scanner.nextLine();
+    System.out.println("Hello, " + name + "!");
   }
 }
