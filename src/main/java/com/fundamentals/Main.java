3828908a89edcbdaae8ce55b9e24edd35d5930ab
Matheus Inocêncio <inolopesm@gmail.com>
2023-07-11 19:06:27 -0300

    create menu

--- src/main/java/com/fundamentals/Main.java
+++ src/main/java/com/fundamentals/Main.java
@@ -1,29 +1,37 @@
 package com.fundamentals;
+
 import java.util.Scanner;
-import com.fundamentals.entity.Person;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        System.out.println("Please insert the number of one option:");
+        System.out.println("[1] Create person [2] List people [3] Update person [4] Delete person [5] Exit");
+        int option = Integer.parseInt(scanner.nextLine());
 
-    try {
-      System.out.println("Please insert your name:");
-      String name = scanner.nextLine();
-      System.out.println("Please insert your age:");
-      int age = Integer.parseInt(scanner.nextLine());
-      Person person = new Person(name, age);
-      System.out.println("Hello, " + person.getName() + " with age " + person.age + "!");
+        if (option == 1) {
+          throw new UnsupportedOperationException("Unimplemented option '1'");
+        }
 
-      if (age < 18) {
-        System.out.println("You are under age!");
-      } else {
-        System.out.println("You are over eighteen!");
+        if (option == 2) {
+          throw new UnsupportedOperationException("Unimplemented option '2'");
+        }
+
+        if (option == 3) {
+          throw new UnsupportedOperationException("Unimplemented option '3'");
+        }
+
+        if (option == 4) {
+          throw new UnsupportedOperationException("Unimplemented option '4'");
+        }
+
+        if (option == 5) {
+          break;
+        }
       }
-
-    } catch (NumberFormatException e) {
-      System.out.println("Ouch! Invalid number");
-    } finally {
-      scanner.close();
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
     }
   }
 }
